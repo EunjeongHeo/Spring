b6791425a1e74664274f92d7a7734002215694b0
EunjeongHeo <munchinthepool64@gmail.com>
2024-02-15 01:22:24 +0900

    .

--- 최태현/library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ 최태현/library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,8 +1,10 @@
 package com.group.libraryapp.controller.calculator;
 
 import com.group.libraryapp.controller.dto.claculator.request.CalculatorAddRequest;
+import com.group.libraryapp.controller.dto.claculator.request.CalculatorMultiplyRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // API 를 개발하려고 하는 클래스 위에 적어주면 이 클래스를 Controller(= API 의 입구)로 등록한다.
@@ -15,8 +17,9 @@ public class CalculatorController {
     }
 
     @PostMapping("/multiply") // POST /multiply
-    public int multiplyTwoNumbers(){
-
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request){
+        return request.getNumber1() * request.getNumber2();
     }
 }
+
 
