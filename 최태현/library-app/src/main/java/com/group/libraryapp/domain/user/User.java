a66e4ca7b06dd93b1fad79b918817fa4efe71950
EunjeongHeo <munchinthepool64@gmail.com>
2024-03-31 19:51:08 +0900

    .

--- 최태현/library-app/src/main/java/com/group/libraryapp/domain/user/User.java
+++ 최태현/library-app/src/main/java/com/group/libraryapp/domain/user/User.java
@@ -34,6 +34,10 @@ public class User {
     public Long getId() {
         return id;
     }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
 }
 
 
--- 최태현/library-app/src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ 최태현/library-app/src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -3,6 +3,7 @@ package com.group.libraryapp.service.user;
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.domain.user.UserRepository;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
 
@@ -28,7 +29,34 @@ public class UserServiceV2 {
                 .collect(Collectors.toList());
     }
 
+    public void updateUser(UserUpdateRequest request) {
+        User user = userRepository.findById(request.getId())
+                .orElseThrow(IllegalArgumentException::new); // 자바 Optional 의 orElseThrow 를 사용
+        user.updateName(request.getName());
+        userRepository.save(user);
+    }
+
+    public void deleteUser(String name){
+        User user = userRepository.findByName(name);
+        if (user == null){
+            throw new IllegalArgumentException();
+        }
+        userRepository.delete(user);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
