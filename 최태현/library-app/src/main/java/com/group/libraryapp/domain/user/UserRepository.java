1cd62cfb87de89d1fe9d5a76924e8125aeaa22b1
Joy <munchinthepool64@gmail.com>
2024-07-30 16:22:11 +0900

    .

--- 최태현/library-app/src/main/java/com/group/libraryapp/domain/user/UserRepository.java
+++ 최태현/library-app/src/main/java/com/group/libraryapp/domain/user/UserRepository.java
@@ -2,10 +2,12 @@ package com.group.libraryapp.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 //@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByName(String name);
+    Optional<User> findByName(String name);
 
 }
 
